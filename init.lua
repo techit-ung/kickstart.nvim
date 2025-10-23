dea9d5ac1ca084b1bd37d11d7277c8411a2fc62a
Techit U <techit.ung@gmail.com>
2025-10-23 21:38:34 +0700

    feat: update blink.cmp keymap to use j/k for tabbing

--- init.lua
+++ init.lua
@@ -842,6 +842,10 @@ require('lazy').setup({
         -- See :h blink-cmp-config-keymap for defining your own keymap
         preset = 'default',
 
+        ['<C-j>'] = { 'select_next', 'fallback' },
+        ['<C-k>'] = { 'select_prev', 'fallback' },
+        ['<C-s>'] = { 'show_signature', 'hide_signature', 'fallback' },
+
         -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
         --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
       },
