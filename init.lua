4b7576874c9682a7f8f1da4fb407b9158c19afb5
Techit U <techit.ungarunyawee@codapayments.com>
2026-01-28 09:43:10 +0700

    feat: add ReloadAs

--- init.lua
+++ init.lua
@@ -303,7 +303,7 @@ require('lazy').setup({
   -- Then, because we use the `opts` key (recommended), the configuration runs
   -- after the plugin has been loaded as `require(MODULE).setup(opts)`.
 
-  { -- Useful plugin to show you pending keybinds.
+  {                     -- Useful plugin to show you pending keybinds.
     'folke/which-key.nvim',
     event = 'VimEnter', -- Sets the loading event to 'VimEnter'
     opts = {
@@ -384,7 +384,7 @@ require('lazy').setup({
       { 'nvim-telescope/telescope-ui-select.nvim' },
 
       -- Useful for getting pretty icons, but requires a Nerd Font.
-      { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
+      { 'nvim-tree/nvim-web-devicons',            enabled = vim.g.have_nerd_font },
     },
     config = function()
       -- Telescope is a fuzzy finder that comes with a lot of different things that
@@ -493,7 +493,7 @@ require('lazy').setup({
       'WhoIsSethDaniel/mason-tool-installer.nvim',
 
       -- Useful status updates for LSP.
-      { 'j-hui/fidget.nvim', opts = {} },
+      { 'j-hui/fidget.nvim',    opts = {} },
 
       -- Allows extra capabilities provided by blink.cmp
       'saghen/blink.cmp',
@@ -1054,6 +1054,15 @@ require('lualine').setup {
     lualine_z = { 'location' },
   },
 }
+
+vim.api.nvim_create_user_command('ReloadAs', function(opts)
+  local ft = opts.fargs[1]
+  vim.bo.filetype = ft
+  vim.cmd 'edit'
+  vim.notify('Reloaded buffer as ' .. ft, vim.log.levels.INFO)
+end, {
+  nargs = 1,
+})
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
