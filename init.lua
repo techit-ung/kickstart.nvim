44a52876122485c6c263c5de6411b11130eeabda
Techit U <techit.ungarunyawee@codapayments.com>
2026-02-09 22:36:14 +0700

    feat: add trim leading and trailing whitespace

--- init.lua
+++ init.lua
@@ -174,6 +174,9 @@ vim.o.shiftwidth = 2
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
+vim.keymap.set('n', '<leader><', ':%s/^\\s\\+//<CR>', { desc = '[<] Clear leading whitespace' })
+vim.keymap.set('n', '<leader>>', ':%s/\\s\\+$//<CR>', { desc = '[>] Clear trailing whitespace' })
+
 -- Clear highlights on search when pressing <Esc> in normal mode
 --  See `:help hlsearch`
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
